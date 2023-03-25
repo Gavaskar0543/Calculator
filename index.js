5d2203504ac7e0f66ba051bf8d85c8901193d913
Gavaskar <gavaskaraeg@gmail.com>
2023-03-25 14:10:36 +0530

    mid commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//fetching display
+let display = document.getElementById('display');
+//fetching buttons;
+let buttons = document.querySelectorAll('#button');
+//variables
+var opeand1 = 0;
+var operand2 = null;
+var operator = null;
+for(var i = 0; i<buttons.length;i++){
+    buttons[i].addEventListener('click',function(){
+    let value = this.getAttribute('data-value');
+     display.innerText += value;
+     if(value =='+'){
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operator = '+';
+
+     }
+    else if(value =='-'){
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operator = '-';
+
+     }
+    else if(value =='/'){
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operator = '/';
+
+     }
+     else if(value =='*'){
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operator = '*';
+
+     }
+     else if(value =='%'){
+        operand1 = parseFloat(display.textContent);
+        display.innerText = "";
+        operator = '%';
+
+     }
+     
+     else if( value == '='){
+        operand2 = parseFloat(display.textContent);
+        display.innerText="";
+        let calcultae = operand1+operator+operand2;
+        let result = eval(calcultae);
+        display.innerText = result;
+     }
+    });
+}
